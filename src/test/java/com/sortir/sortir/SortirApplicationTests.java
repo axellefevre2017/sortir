128d982496e455ac0d48e2277ddc7ec5674080de
axlefevre <a.lefevre@agrial.com>
2018-10-01 10:08:08 +0200

    Initial commit

---
+++ src/test/java/com/sortir/sortir/SortirApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sortir.sortir;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SortirApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
