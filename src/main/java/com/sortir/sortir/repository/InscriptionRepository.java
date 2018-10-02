919cf4a494232c7eaa3a179e7d43993f679ebcdd
axlefevre <a.lefevre@agrial.com>
2018-10-02 13:18:43 +0200

    Mise en page ville

--- src/main/java/com/sortir/sortir/repository/InscriptionRepository.java
+++ src/main/java/com/sortir/sortir/repository/InscriptionRepository.java
@@ -1,8 +1,11 @@
 package com.sortir.sortir.repository;
 
 import com.sortir.sortir.entity.Inscription;
+import com.sortir.sortir.entity.Sortie;
 import com.sortir.sortir.entity.pk.InscriptionPk;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
--- src/main/java/com/sortir/sortir/repository/VilleRepository.java
+++ src/main/java/com/sortir/sortir/repository/VilleRepository.java
@@ -1,8 +1,9 @@
 package com.sortir.sortir.repository;
 
-import com.sortir.sortir.entity.Participant;
 import com.sortir.sortir.entity.Ville;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,5 +13,7 @@ public interface VilleRepository extends JpaRepository<Ville, Integer> {
 
     @Override
     List<Ville> findAll();
+
+    List<Ville> findAllByVilleContaining(String ville);
 
 }
