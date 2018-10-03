55501acf142b2c0eb7e20a8afc3796b830a810a1
axlefevre <a.lefevre@agrial.com>
2018-10-03 14:38:00 +0200

    Tableau complet à l'accueil

--- src/main/java/com/sortir/sortir/repository/InscriptionRepository.java
+++ src/main/java/com/sortir/sortir/repository/InscriptionRepository.java
@@ -1,6 +1,7 @@
 package com.sortir.sortir.repository;
 
 import com.sortir.sortir.entity.Inscription;
+import com.sortir.sortir.entity.Participant;
 import com.sortir.sortir.entity.Sortie;
 import com.sortir.sortir.entity.pk.InscriptionPk;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,4 +16,9 @@ public interface InscriptionRepository extends JpaRepository<Inscription, Inscri
 
     List<Inscription> findAll();
 
+    @Query(value="SELECT COUNT(*) FROM Inscriptions a, Sorties b, Participants c where a.sorties_no_sortie=b.no_sortie and c.no_participant=a.participants_no_participant and a.sorties_no_sortie = :sortie and a.participants_no_participant = :organisateur", nativeQuery = true)
+    Integer countBySortieAndParticipant(@Param("sortie") Integer sortie, @Param("organisateur") Integer organisateur);
+
+    @Query(value="SELECT COUNT(*) FROM Inscriptions a, Sorties b where a.sorties_no_sortie=b.no_sortie and a.sorties_no_sortie = :sortie", nativeQuery = true)
+    Integer countBySortie(@Param("sortie") Integer sortie);
 }
--- src/main/java/com/sortir/sortir/repository/SortieRepository.java
+++ src/main/java/com/sortir/sortir/repository/SortieRepository.java
@@ -1,9 +1,12 @@
 package com.sortir.sortir.repository;
 
+import com.sortir.sortir.entity.Site;
 import com.sortir.sortir.entity.Sortie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,4 +16,6 @@ public interface SortieRepository extends JpaRepository<Sortie, Integer> {
     List<Sortie> findAll();
 
     Optional<Sortie> findById(Integer id);
+
+    //List<Sortie> filter(@Param("nom") String nom, Site site, Date debut, Date fin,);
 }
